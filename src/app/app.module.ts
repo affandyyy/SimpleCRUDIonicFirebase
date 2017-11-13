eed6e4fb941f635315f04af6f7a8066071770071
Syahmi Affandy <affandyyy@Affandys-MacBook-Pro.local>
2017-11-13 13:00:25 +0800

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+ 
+// Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { BrowserModule } from '@angular/platform-browser';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+
+ 
+// AF2 Settings
+export const firebaseConfig = {
+  apiKey: "AIzaSyD9ZgeM-CZPU9YF5Fm86VMODnz_7V_su7g",
+  authDomain: "myionic-88f63.firebaseapp.com",
+  databaseURL: "https://myionic-88f63.firebaseio.com",
+  storageBucket: "myionic-88f63.appspot.com",
+  messagingSenderId: "768831429494"
+};
 
 @NgModule({
   declarations: [
@@ -13,8 +28,13 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp), BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+
+    
+    
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -25,6 +45,7 @@ import { HomePage } from '../pages/home/home';
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
+    
   ]
 })
 export class AppModule {}
